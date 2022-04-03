143f74901f436440ed6929e656b635a081d5bc90
MaksimMurzin <maks_murzin@yahoo.com>
2022-04-03 22:58:56 +0100

    added some useful comments

--- TextFilter/Program.cs
+++ TextFilter/Program.cs
@@ -2,19 +2,36 @@
 using System.Text.RegularExpressions;
 using TextFilter;
 
-var charsToRemove = new string[] { "@", ",", ".", ";", "'","`",":" };
+// you can add more chars here which can be rejected
+var charsToRemove = new string[] { "@", ",", ".", ";", "'","`",":", "(", ")" };
 var vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
 string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\data_file.txt");
 
 
+/// <summary>
+/// this is still not perfect, there are some more edgecases 
+/// that I could come up with but it would go beyond the scope of this project
+/// </summary>
 IDataReader dataReader = (SimpleFactory.GetDataReader(path, charsToRemove) as DataReader);
+var data = dataReader.GetData().ToList();
+//foreach(var word in data)
+//{
+//    Console.WriteLine(word);
+//}
 
 
+// this is an example of how to use the OperationHandler, swap out the delegate for the other functions I made or add your own
+//var filteredWords = OperationHandler.FilterWordsWith(data, OperationHandler.IsWordLongerOrEqualTo, 4).ToList();
+
+//foreach(var word in filteredWords)
+//{
+//    Console.WriteLine(word);
+//}
 
 
-var data = "get out again.The rabbit hole".Split(' ');
+//var data = "get out again.The rabbit hole".Split(' ');
 
-var test = (dataReader as DataReader).EdgeCaseProblem(data);
+//var test = (dataReader as DataReader).EdgeCaseProblem(data);
 // ======== edge case ===============================
 // words where there is a dot without a space throw an error
 
